0c7ea9ce7aed3095895fb419f906537d13efc7c4
rajesha <rajesh.ayyavu@object-frontier.com>
2014-11-25 19:38:09 +0530

    commented the code for hard coded password

--- javascript/index.js
+++ javascript/index.js
@@ -5,10 +5,10 @@ function validateLogin() {
 
 	var _username = "rajesha";
 	var _password = "test123";
-	//var username = document.forms["login_form"]["username"].value;
-	//var password = document.forms["login_form"]["password"].value;
-	username = _username;
-	password = _password;
+	var username = document.forms["login_form"]["username"].value;
+	var password = document.forms["login_form"]["password"].value;
+	//username = _username;
+	//password = _password;
 	
 	if(username.length == 0 || password.length == 0) {
 		alert("User name or password should not be empty");
